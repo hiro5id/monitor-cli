b18b6da69a30effbd8c0822b2b96ac4304ca95ce
Roberto Sebestyen <roberto@sebestyen.ca>
2022-01-19 02:21:52 -0500

    host it over http

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { MonitorControl } from './platforms/monitor-control';
 import { Linux } from './platforms/linux';
 import { Windows } from './platforms/windows';
 import { Darwin } from './platforms/darwin';
+import * as http from 'http';
+import { IncomingMessage, ServerResponse } from 'http';
 
 export async function main() {
   const platformSpecificMonitors: MonitorControl[] = [new Linux(), new Windows(), new Darwin()];
@@ -12,16 +14,34 @@ export async function main() {
     throw new Error(`This platform [${platformSpecificMonitors[0].platformName}]  is not supported`);
   }
 
-  console.log('going to sleep');
-  await thisPlatform.sleep();
+  const host = 'localhost';
+  const port = 8083;
 
-  console.log('waiting');
-  await new Promise(resolve => {
-    setTimeout(() => {
-      resolve('done');
-    }, 3000);
+  const requestListener = async function (req: IncomingMessage, res: ServerResponse) {
+    res.writeHead(200);
+
+    if (req.url?.toLowerCase().startsWith('/on')) {
+      res.end(`turning monitor on`);
+      await thisPlatform.wake();
+    } else if (req.url?.toLowerCase().startsWith('/off')) {
+      res.end(`turning monitor off`);
+      await thisPlatform.sleep();
+    } else {
+      res.end(`thanks for coming by`);
+    }
+  };
+
+  const server = http.createServer(requestListener);
+  server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
   });
 
-  console.log('waking');
-  await thisPlatform.wake();
+  // noinspection InfiniteLoopJS
+  while (true) {
+    await new Promise(resolve => {
+      setTimeout(() => {
+        resolve(null);
+      }, 10000);
+    });
+  }
 }
